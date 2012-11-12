0bac9a265e6b44da443e78d0f1c817a58207fcfa
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-11-11 21:01:24 -0500

    nxp: fix self-extractors
    Change-Id: I9bf0daecbac98de3301595bbe4354b61ddb8bb33

--- self-extractors/nxp/staging/device-partial.mk
+++ self-extractors/nxp/staging/device-partial.mk
@@ -13,5 +13,8 @@
 # limitations under the License.
 
 # NXP blob(s) necessary for Toro hardware
-PRODUCT_PACKAGES := \
-    libpn544_fw
+LOCAL_PATH := vendor/nxp/toro
+
+# NXP blob necessary for Verizon Galazy Nexus hardware
+PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/proprietary/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so
